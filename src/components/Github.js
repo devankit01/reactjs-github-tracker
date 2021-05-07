cd7afa010abef9b3698b764e11c9a66251537f45
devankit01 <devankitgupta01@gmail.com>
2021-05-07 23:15:20 +0530

    added

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,15 +1,31 @@
 import React from "react";
-
+import { useState } from "react";
 function Github() {
+  const [search, setSearch] = useState("devankit01");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form");
+    getData();
+  };
+
+  const getData = async (search) => {
+    const response = await fetch(`https://api.github.com/users/${search}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div>
       <div className="git__section">
         <div className="git__search">
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="search"
-              value=""
-              onChange=""
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
               placeholder="Enter Github User"
             ></input>
             <button type="submit">Search</button>
@@ -41,7 +57,7 @@ function Github() {
 
             <div className="info__card">
               <div className="info__content__icon">
-              <i class="far fa-user"></i>
+                <i class="far fa-user"></i>
               </div>
               <div className="info__content__text">
                 <h3>2</h3>
@@ -86,28 +102,33 @@ function Github() {
               </span>
 
               <div className="user__extra">
-               
-                <p><i class="far fa-building"></i>&nbsp;Coding Addict</p>
-                <p><i class="fas fa-map-marker-alt"></i>&nbsp;Lucknow, India</p>
-                <p><i class="fas fa-paperclip"></i>&nbsp;www.medev.in</p>
+                <p>
+                  <i class="far fa-building"></i>&nbsp;Coding Addict
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i>&nbsp;Lucknow, India
+                </p>
+                <p>
+                  <i class="fas fa-paperclip"></i>&nbsp;www.medev.in
+                </p>
               </div>
               <button className="btn">Follow</button>
-
             </div>
           </div>
           <div className="user__repos">
             <h4>Repositories</h4>
             <div className="repos">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laborum, est! Incidunt similique culpa unde, excepturi quas
-                aliquid! Dolore corporis repellendus eveniet voluptatem fugiat
-                perspiciatis dignissimos ullam earum labore, dolorem recusandae!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Laborum, est! Incidunt similique culpa unde, excepturi quas
-                aliquid! Dolore corporis repellendus eveniet voluptatem fugiat
-                perspiciatis dignissimos ullam earum labore, dolorem recusandae!
-              </p>
+              <div className="repo__card">
+                <div className="icon">
+                  <i class="fab fa-codepen"></i>
+                </div>
+                <div className="repo__info">
+                  <h5>30daysofcode</h5>
+                  <a href="" className="text_light">
+                    Clone now &rarr;
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
