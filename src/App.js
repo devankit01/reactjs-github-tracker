b0c25b25840f629b1f3e92979f0664066f249910
devankit01 <devankitgupta01@gmail.com>
2021-05-07 22:22:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Github  from "./components/Github";
+import Error  from "./components/Error";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav>
+        <p>ReactJS Social Media Tracker</p>
+      </nav>
+
+      <BrowserRouter>
+
+      <Switch>
+        <Route exact path="/">
+    
+      <div className="social__container">
+        <a href="/github">
+          <div className="social__card">
+           <div className="social__icon">  <i class="fa fa-github"></i></div><p>Github</p>
+          </div>
         </a>
-      </header>
+
+        <a href="/linkdein">
+          <div className="social__card">
+          <div className="social__icon">  <i class="fa fa-linkedin"></i></div><p>LinkedIn</p>
+          </div>
+     
+        </a>
+
+        <a href="/instagram">
+          <div className="social__card">
+          <div className="social__icon">  <i class="fa fa-instagram"></i></div><p>Instagram</p>
+          </div>
+        </a>
+
+        <a href="/facebook">
+          <div className="social__card">
+          <div className="social__icon">  <i class="fa fa-facebook"></i></div><p>Facebook</p>
+          </div>
+        </a>
+      </div>
+        </Route>
+      </Switch>
+        <Switch>
+          <Route path="/github" component={Github}>
+            <Github />
+          </Route>
+        </Switch>
+
+        {/* <Switch>
+          <Route  component={Error}>
+            <Error/>
+          </Route>
+        </Switch> */}
+
+      </BrowserRouter>
     </div>
   );
 }
