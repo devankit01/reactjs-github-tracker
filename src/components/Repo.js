78402dde2be6d73d0e3d778b2e7a70d903b767ee
devankit01 <devankitgupta01@gmail.com>
2021-07-14 09:41:32 +0530

    Added

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 function Repo({ name }) {
@@ -20,26 +20,25 @@ function Repo({ name }) {
 }
 
 export default Repo;
-=======
-import React from "react";
+// import React from "react";
 
-function Repo({ name }) {
-  return (
-      <>
-    <div className="repo__card">
-      <div className="icon">
-        <i class="fab fa-codepen"></i>
-      </div>
-      <div className="repo__info">
-        <h5>{name}</h5>
-        <a href="" className="text_light">
-          Clone now &rarr;
-        </a>
-      </div>
-    </div>
-    </>
-  );
-}
+// function Repo({ name }) {
+//   return (
+//       <>
+//     <div className="repo__card">
+//       <div className="icon">
+//         <i class="fab fa-codepen"></i>
+//       </div>
+//       <div className="repo__info">
+//         <h5>{name}</h5>
+//         <a href="" className="text_light">
+//           Clone now &rarr;
+//         </a>
+//       </div>
+//     </div>
+//     </>
+//   );
+// }
 
-export default Repo;
->>>>>>> 8a2959b0229a078691fb54322870c16128302314
+// export default Repo;
+
